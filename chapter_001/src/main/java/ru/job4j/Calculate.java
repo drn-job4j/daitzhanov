2b973f42a3016488db55aec3c8474c532a09d13b
Darkhan Aitzhanov <drn.job4j@gmail.com>
2017-01-08 00:43:58 +0600

    add after Maven,JavaDoc,JUnit

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+/**
+ * Класс для вычисление арифметических операций + - * / ^.
+ * @author daitzhanov
+ * @since 07.01.2017
+ * @version 1.0
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Test for Calculate.java
+ *
+ * @author daitzhanov
+ * @version 1.0
+ * @since 07.01.2017
+ */
+ 
+public class CalculateTest {
+	/**
+	 * Test add.
+	 */
+	 @Test
+	 public void whenExecuteMainThenPrintToConsole() {
+		 ByteArrayOutputStream out = new ByteArrayOutputStream();
+		 System.setOut(new PrintStream(out));
+		 Calculate.main(null);
+		 assertThat(
+				out.toString(),
+				is(
+						String.format(
+								"Hello World%s",
+								System.getProperty("line.separator")
+						)
+				)
+		 );
+	 }
+}
